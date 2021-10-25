6b401780858ed10daa813150079881cf37b29240
mariabendix1 <ljs414@alumni.ku.dk>
2021-10-25 10:46:52 +0200

    Rectangle example

---
+++ lecture6/Rectangle.java
@@ -0,0 +1,14 @@
+class Rectangle{
+
+  int width;
+  int height;
+
+  public Rectangle(int width, int height){
+    this.width = width;
+    this.height = height;
+  }
+
+  int getArea(){
+    return width * height;
+  }
+}
---
+++ lecture6/RectangleMain.java
@@ -0,0 +1,8 @@
+public class RectangleMain {
+  public static void main(String[] args){
+    Rectangle r = new Rectangle(30,27);
+    Rectangle r1 = new Rectangle(34,1);
+    System.out.println(r.getArea());
+    System.out.println(r1.getArea());
+  }
+}
